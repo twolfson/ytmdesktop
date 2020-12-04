918cdd2db87cedfb872a834402d640146d80979e
Todd Wolfson <todd@twolfson.com>
2020-12-03 18:59:00 -0800

    Attempting to find out why tooltip isn't working =/

--- src/providers/trayProvider.js
+++ src/providers/trayProvider.js
@@ -15,11 +15,14 @@ let contextMenu = null
 let iconTray = assetsProvider.getIcon('trayTemplate')
 
 function setTooltip(tooltip) {
+    console.log('seeet tooltip', tooltip);
     try {
         if (tray) {
-            tray.setToolTip(tooltip)
+            console.log('wat', tray.setToolTip(tooltip))
+            console.log('bleh');
         }
     } catch (error) {
+        console.log('errror');
         ipcMain.emit('log', {
             type: 'warn',
             data: `Failed to setTooltip: ${error}`,
@@ -69,106 +72,106 @@ function createTray(mainWindow) {
 }
 
 function updateTray(data) {
-    try {
-        if (tray) {
-            var template = popUpMenu(__, saved_mainWindow, mediaControl, app)
+    // try {
+    //     if (tray) {
+    //         var template = popUpMenu(__, saved_mainWindow, mediaControl, app)
 
-            if (data.type == 'audioOutputs') {
-                template[11]['submenu'] = data.data
-            }
+    //         if (data.type == 'audioOutputs') {
+    //             template[11]['submenu'] = data.data
+    //         }
 
-            contextMenu = Menu.buildFromTemplate(template)
+    //         contextMenu = Menu.buildFromTemplate(template)
 
-            tray.setContextMenu(contextMenu)
-        }
-    } catch (error) {
-        ipcMain.emit('log', {
-            type: 'warn',
-            data: `Failed to updateTray: ${error}`,
-        })
-    }
+    //         tray.setContextMenu(contextMenu)
+    //     }
+    // } catch (error) {
+    //     ipcMain.emit('log', {
+    //         type: 'warn',
+    //         data: `Failed to updateTray: ${error}`,
+    //     })
+    // }
 }
 
 function updateTrayIcon(icon) {
-    try {
-        if (tray) {
-            nativeImageIcon = buildTrayIcon(icon)
-            tray.setImage(nativeImageIcon)
-        }
-    } catch (error) {
-        ipcMain.emit('log', {
-            type: 'warn',
-            data: `Failed to updateTrayIcon: ${error}`,
-        })
-    }
+    // try {
+    //     if (tray) {
+    //         nativeImageIcon = buildTrayIcon(icon)
+    //         tray.setImage(nativeImageIcon)
+    //     }
+    // } catch (error) {
+    //     ipcMain.emit('log', {
+    //         type: 'warn',
+    //         data: `Failed to updateTrayIcon: ${error}`,
+    //     })
+    // }
 }
 
 function buildTrayIcon(icon) {
-    let nativeImageIcon = nativeImage.createFromPath(icon)
-    return nativeImageIcon
+    // let nativeImageIcon = nativeImage.createFromPath(icon)
+    // return nativeImageIcon
 }
 
 function balloon(title, content, cover, icon) {
-    if (title && content && cover) {
-        imageToBase64(cover)
-            .then((response) => {
-                _doNotification(
-                    title,
-                    content,
-                    nativeImage.createFromDataURL(
-                        `data:image/jpeg;base64,${response}`
-                    )
-                )
-            })
-            .catch((_) => {
-                _doNotification(
-                    title,
-                    content,
-                    nativeImage.createFromPath(icon)
-                )
-            })
-    }
+    // if (title && content && cover) {
+    //     imageToBase64(cover)
+    //         .then((response) => {
+    //             _doNotification(
+    //                 title,
+    //                 content,
+    //                 nativeImage.createFromDataURL(
+    //                     `data:image/jpeg;base64,${response}`
+    //                 )
+    //             )
+    //         })
+    //         .catch((_) => {
+    //             _doNotification(
+    //                 title,
+    //                 content,
+    //                 nativeImage.createFromPath(icon)
+    //             )
+    //         })
+    // }
 }
 
 function balloonEvents(data) {
-    _doNotification(data.title, data.content, data.icon)
+    // _doNotification(data.title, data.content, data.icon)
 }
 
 function _doNotification(title, content, icon) {
-    try {
-        if (title && content) {
-            if (systemInfo.isWindows()) {
-                if (!settingsProvider.get('settings-tray-icon')) {
-                    nativeImageIcon = buildTrayIcon(iconTray)
-                    tray = new Tray(nativeImageIcon)
-                }
-                tray.displayBalloon({
-                    icon: icon,
-                    title: title,
-                    content: content,
-                    noSound: true,
-                })
-                if (!settingsProvider.get('settings-tray-icon')) {
-                    setTimeout(() => {
-                        quit()
-                    }, 7 * 1000)
-                }
-            } else {
-                let Notification = require('electron-native-notification')
+    // try {
+    //     if (title && content) {
+    //         if (systemInfo.isWindows()) {
+    //             if (!settingsProvider.get('settings-tray-icon')) {
+    //                 nativeImageIcon = buildTrayIcon(iconTray)
+    //                 tray = new Tray(nativeImageIcon)
+    //             }
+    //             tray.displayBalloon({
+    //                 icon: icon,
+    //                 title: title,
+    //                 content: content,
+    //                 noSound: true,
+    //             })
+    //             if (!settingsProvider.get('settings-tray-icon')) {
+    //                 setTimeout(() => {
+    //                     quit()
+    //                 }, 7 * 1000)
+    //             }
+    //         } else {
+    //             let Notification = require('electron-native-notification')
 
-                new Notification(title, {
-                    body: content,
-                    silent: true,
-                    icon: icon,
-                })
-            }
-        }
-    } catch (error) {
-        ipcMain.emit('log', {
-            type: 'warn',
-            data: `Failed to _doNotification: ${error}`,
-        })
-    }
+    //             new Notification(title, {
+    //                 body: content,
+    //                 silent: true,
+    //                 icon: icon,
+    //             })
+    //         }
+    //     }
+    // } catch (error) {
+    //     ipcMain.emit('log', {
+    //         type: 'warn',
+    //         data: `Failed to _doNotification: ${error}`,
+    //     })
+    // }
 }
 
 function quit() {
@@ -176,33 +179,33 @@ function quit() {
 }
 
 function setShinyTray() {
-    try {
-        if (settingsProvider.get('settings-shiny-tray') && systemInfo.isMac()) {
-            tray.setContextMenu(null)
-            tray.removeAllListeners()
-            tray.on('right-click', (event, bound, position) => {
-                tray.popUpContextMenu(contextMenu)
-            })
-            tray.on('click', (event, bound, position) => {
-                if (position.x < 32) {
-                    saved_mainWindow.show()
-                } else if (position.x > 130) {
-                    mediaControl.playPauseTrack(
-                        saved_mainWindow.getBrowserView()
-                    )
-                }
-            })
-        } else {
-            // Shiny tray disabled ||| on onther platform
-            tray.removeAllListeners()
-            init_tray()
-        }
-    } catch (error) {
-        ipcMain.emit('log', {
-            type: 'warn',
-            data: `Failed to setShinyTray: ${error}`,
-        })
-    }
+    // try {
+    //     if (settingsProvider.get('settings-shiny-tray') && systemInfo.isMac()) {
+    //         tray.setContextMenu(null)
+    //         tray.removeAllListeners()
+    //         tray.on('right-click', (event, bound, position) => {
+    //             tray.popUpContextMenu(contextMenu)
+    //         })
+    //         tray.on('click', (event, bound, position) => {
+    //             if (position.x < 32) {
+    //                 saved_mainWindow.show()
+    //             } else if (position.x > 130) {
+    //                 mediaControl.playPauseTrack(
+    //                     saved_mainWindow.getBrowserView()
+    //                 )
+    //             }
+    //         })
+    //     } else {
+    //         // Shiny tray disabled ||| on onther platform
+    //         tray.removeAllListeners()
+    //         init_tray()
+    //     }
+    // } catch (error) {
+    //     ipcMain.emit('log', {
+    //         type: 'warn',
+    //         data: `Failed to setShinyTray: ${error}`,
+    //     })
+    // }
 }
 
 function updateImage(payload) {
